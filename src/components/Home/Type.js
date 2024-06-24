75dbfae2e0aa808fd107adbd3151d26b27cb8a05
happydev0105 <lptop009@gmail.com>
2024-06-24 20:45:49 +0900

    refer to me

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -9,7 +9,7 @@ function Type() {
           "Software Developer",
           "Freelancer",
           "MERN Stack Developer",
-          "Open Source Contributor",
+          "SEO Developer",
         ],
         autoStart: true,
         loop: true,
